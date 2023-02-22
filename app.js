2378c8598fc2e52e3517459f207c44684c1244a1
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-02-22 13:54:34 +0530

    first api

--- app.js
+++ app.js
@@ -3,9 +3,30 @@ const http = require('http');
 http
   .createServer((req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    // fetch data from database
-    const data = '{"name": "akhil"}';
 
-    res.end(data);
+    if (req.method === 'GET') {
+      res.end(JSON.stringify({ loggedIn: 'false', method: 'GET' }));
+    }
+
+    if (req.method === 'POST') {
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      req.on('end', () => {
+        console.log(body);
+        const parsedBody = JSON.parse(body);
+
+        // this is from database
+        const password = 'iwillhack';
+
+        if (parsedBody.password !== password) {
+          res.end(JSON.stringify({ loggedIn: 'false', method: 'POST' }));
+        }
+
+        res.end(JSON.stringify({ loggedIn: 'true', method: 'POST' }));
+      });
+    }
   })
   .listen(5005);
